e4d09468c0ddedafcd1c82ab791747a14b0fad2f
zdoh <denfairycat@gmail.com>
2019-08-05 21:31:18 +0300

    Initial commit

---
+++ src/main/java/ru/zdoher/japs/JapanSentenseApplication.java
@@ -0,0 +1,13 @@
+package ru.zdoher.japs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapanSentenseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JapanSentenseApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ru/zdoher/japs/Main.java
@@ -0,0 +1,8 @@
+package ru.zdoher.japs;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Aaa");
+    }
+}
