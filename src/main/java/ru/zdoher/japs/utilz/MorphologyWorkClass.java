d65b462dc0601557702b04ff082a09d6d2336265
zdoh <denfairycat@gmail.com>
2019-10-08 19:21:07 +0300

    v0.3.4.1 change - some small change

--- src/main/java/ru/zdoher/japs/utilz/MorphologyWorkClass.java
+++ src/main/java/ru/zdoher/japs/utilz/MorphologyWorkClass.java
@@ -1,6 +1,10 @@
 package ru.zdoher.japs.utilz;
 
+import com.atilika.kuromoji.ipadic.Token;
+import com.atilika.kuromoji.ipadic.Tokenizer;
 import ru.zdoher.japs.domain.sentence.Sentence;
+
+import java.util.List;
 
 public class MorphologyWorkClass {
 
@@ -23,8 +27,27 @@ public class MorphologyWorkClass {
     public void morphologyTest() {
         Sentence sentence = new Sentence();
         sentence.setSentence("私は会社員です。");
-
         System.out.println(sentence);
+
+        getAllToken(sentence);
+
     }
 
+
+    private void getAllToken(Sentence sentence) {
+        Tokenizer tokenizer = new Tokenizer() ;
+        List<Token> tokens = tokenizer.tokenize(sentence.getSentence());
+
+
+
+        for (Token token : tokens) {
+            System.out.println(token.getSurface() + "\t" +
+                    token.getPartOfSpeechLevel1() + "|" +
+                    token.getPartOfSpeechLevel2() + "|" +
+                    token.getPartOfSpeechLevel3() + "|" +
+                    token.getPartOfSpeechLevel4() + "|" +
+                    token.getConjugationType() + "|" +
+                    token.getAllFeatures());
+        }
+    }
 }
